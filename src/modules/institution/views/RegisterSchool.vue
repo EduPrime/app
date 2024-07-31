170ac0349356a305ebb5db41ec62dcdccb896257
Balkishan Dhaker <balkishandhaker97@gmail.com>
2024-07-31 17:33:55 +0530

    Yup Form implementation with Validation and masking.

--- src/modules/institution/views/RegisterSchool.vue
+++ src/modules/institution/views/RegisterSchool.vue
@@ -1,34 +1,82 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { IonContent, IonLabel, IonPage, IonSegment, IonSegmentButton } from '@ionic/vue'
-
+import { ref } from 'vue';
+import { IonLabel, IonSegment, IonSegmentButton } from '@ionic/vue';
+import * as yup from 'yup';
+import { useForm } from 'vee-validate';
+import EpInput from '@/components/EpInput.vue';
 const selectedSegment = ref('general-info')
 
-const form = ref({
-  id: '',
-  name: '',
-  address: '',
-  city: '',
-  state: '',
-  postalCode: '',
-  phone: '',
-  abbreviation: '',
-  longitude: '',
-  latitude: '',
-  totalArea: '',
-  builtArea: '',
-  availableArea: '',
-  acronym: '',
-  logoUrl: '',
-  managerId: '',
-  managerPosition: '',
-  operationLocation: '',
-  numberOfFloors: '',
-})
+const formSchema = yup.object({
+  id: yup
+    .number()
+    .typeError('ID deve ser numérico')
+    .required('ID é obrigatório'),
+  name: yup.string().required('Nome é obrigatório'),
+  abbreviation: yup.string().required('Abreviação é obrigatória'),
+  managerId: yup.string().required('ID do Gerente é obrigatório'),
+  managerPosition: yup.string().required('Posição do Gerente é obrigatória'),
+  logoUrl: yup.string().url('URL inválida').required('URL do Logo é obrigatória'),
+  address: yup.string().required('Endereço é obrigatório'),
+  city: yup.string().required('Cidade é obrigatória'),
+  state: yup.string().required('Estado é obrigatório'),
+  postalCode: yup
+    .string()
+    .required('CEP é obrigatório')
+    .matches(/^\d{5}-\d{3}$/, 'CEP inválido'),
+  phone: yup
+    .string()
+    .required('Telefone é obrigatório')
+    .matches(/^\(\d{2}\) \d{5}-\d{4}$/, 'Telefone inválido'),
+  latitude: yup
+    .number()
+    .required('Latitude é obrigatória')
+    .min(-90, 'Latitude deve ser entre -90 e 90')
+    .max(90, 'Latitude deve ser entre -90 e 90'),
+  longitude: yup
+    .number()
+    .required('Longitude é obrigatória')
+    .min(-180, 'Longitude deve ser entre -180 e 180')
+    .max(180, 'Longitude deve ser entre -180 e 180'),
+  numberOfFloors: yup
+    .number()
+    .required('Número de Andares é obrigatório')
+    .min(0, 'Número de Andares deve ser um número positivo')
+    .max(99, 'Número de Andares deve ter no máximo 2 dígitos'),
+  totalArea: yup
+    .number()
+    .required('Área Total é obrigatória')
+    .min(0, 'Área Total deve ser um número positivo'),
+  builtArea: yup
+    .number()
+    .required('Área Construída é obrigatória')
+    .min(0, 'Área Construída deve ser um número positivo'),
+  availableArea: yup
+    .number()
+    .required('Área Disponível é obrigatória')
+    .min(0, 'Área Disponível deve ser um número positivo'),
+  operationLocation: yup.string().required('Local de Operação é obrigatório')
+});
+
+const { handleSubmit, resetForm, values, errors } = useForm({
+  validationSchema: formSchema,
+});
 
 function registerSchool() {
-  console.log('Escola registrada:', form.value)
+  // console.log('Escola registrada:', form.value);
+
+  /** Custom Validations */
+  console.log("Values : ", values);
+  console.log("Errors : ", errors.value);
+  handleSubmit((handleFormData)=>{
+      console.log("Handle Submission Data: ", handleFormData);
+  })
 }
+
+//** Mask Inputs
+const idMask = ref([/\d/, /\d/, /\d/, /\d/, /\d/,  /\d/]);
+const postalCodeMask = ref([/\d/, /\d/, /\d/, /\d/, /\d/,"-",  /\d/,/\d/,/\d/]);
+const phoneMask = ref(["(",/\d/,/\d/,")"," ", /\d/, /\d/, /\d/, /\d/, /\d/," ",  /\d/,/\d/,/\d/,/\d/]);
+const areaMask = ref([/\d/,/\d/,/\d/,/\d/,/\d/]);
 </script>
 
 <template>
@@ -64,108 +112,29 @@ function registerSchool() {
     </ion-segment>
 
     <div v-show="selectedSegment === 'general-info'">
-      <ion-item>
-        <ion-label position="stacked">
-          Nome
-        </ion-label>
-        <ion-input v-model="form.name" placeholder="Digite o nome da escola" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Abreviação
-        </ion-label>
-        <ion-input v-model="form.abbreviation" placeholder="Digite a abreviação" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          ID do Gerente
-        </ion-label>
-        <ion-input v-model="form.managerId" placeholder="Digite o ID do gerente" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Posição do Gerente
-        </ion-label>
-        <ion-input v-model="form.managerPosition" placeholder="Digite a posição do gerente" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          URL do Logo
-        </ion-label>
-        <ion-input v-model="form.logoUrl" placeholder="Digite a URL do logo" />
-      </ion-item>
+      <EpInput name="name" label="Nome" placeholder="Digite o nome da escola" />
+      <EpInput name="abbreviation" label="Abreviação" placeholder="Digite a abreviação" />
+      <EpInput name="managerId" :mask="idMask" label="ID do Gerente" placeholder="999999" />
+      <EpInput name="managerPosition" label="Posição do Gerente" placeholder="Digite a posição do gerente" />
+      <EpInput name="logoUrl" label="URL do Logo" placeholder="Digite a URL do logo" />
     </div>
 
     <div v-show="selectedSegment === 'location'">
-      <ion-item>
-        <ion-label position="stacked">
-          Endereço
-        </ion-label>
-        <ion-input v-model="form.address" placeholder="Digite o endereço" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Cidade
-        </ion-label>
-        <ion-input v-model="form.city" placeholder="Digite a cidade" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Estado
-        </ion-label>
-        <ion-input v-model="form.state" placeholder="Digite o estado" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          CEP
-        </ion-label>
-        <ion-input v-model="form.postalCode" placeholder="Digite o CEP" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Latitude
-        </ion-label>
-        <ion-input v-model="form.latitude" placeholder="Digite a latitude" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Longitude
-        </ion-label>
-        <ion-input v-model="form.longitude" placeholder="Digite a longitude" />
-      </ion-item>
+      <EpInput name="address" label="Endereço" placeholder="Digite o endereço" />
+      <EpInput name="city" label="Cidade" placeholder="Digite a cidade" />
+      <EpInput name="state" label="Estado" placeholder="Digite o estado" />
+      <EpInput name="postalCode" :mask="postalCodeMask" inputmode="number" label="CEP" placeholder="99999-999" />
+      <EpInput name="phone" :mask="phoneMask" inputmode="tel" label="Phone" placeholder="(99) 99999-9999" />
+      <EpInput name="latitude" label="Latitude" type="number" placeholder="Digite a latitude" />
+      <EpInput name="longitude" label="Longitude" type="number" placeholder="Digite a longitude" />
     </div>
 
     <div v-show="selectedSegment === 'facilities'">
-      <ion-item>
-        <ion-label position="stacked">
-          Número de Andares
-        </ion-label>
-        <ion-input v-model="form.numberOfFloors" placeholder="Digite o número de andares" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Área Total
-        </ion-label>
-        <ion-input v-model="form.totalArea" placeholder="Digite a área total" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Área Construída
-        </ion-label>
-        <ion-input v-model="form.builtArea" placeholder="Digite a área construída" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Área Disponível
-        </ion-label>
-        <ion-input v-model="form.availableArea" placeholder="Digite a área disponível" />
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">
-          Local de Operação
-        </ion-label>
-        <ion-input v-model="form.operationLocation" placeholder="Digite o local de operação" />
-      </ion-item>
+      <EpInput name="numberOfFloors" :mask="[/\d/,/\d/]" label="Número de Andares" placeholder="99" />
+      <EpInput name="totalArea" :mask="areaMask" label="Área Total" placeholder="99999" />
+      <EpInput name="builtArea" :mask="areaMask" label="Área Construída" placeholder="99999" />
+      <EpInput name="availableArea" :mask="areaMask" label="Área Disponível" placeholder="99999" />
+      <EpInput name="operationLocation" label="Local de Operação" placeholder="Digite o local de operação" />
     </div>
     <template #footer>
       <ion-grid>
@@ -185,11 +154,3 @@ function registerSchool() {
     </template>
   </content-layout>
 </template>
-
-<style scoped>
-/* Estilos opcionais */
-</style>
-
-<style scoped>
-/* Estilos opcionais */
-</style>
