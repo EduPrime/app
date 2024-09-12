0ac1f3986ab8009ef31ae0c030a8dd54647fb272
hermesalvesbr <hermes.alves@araripina.com.br>
2024-09-12 16:50:42 -0300

    chore: new icons

--- src/modules/school-management/routes.ts
+++ src/modules/school-management/routes.ts
@@ -1,16 +1,9 @@
-import { school } from 'ionicons/icons'
+import { create, pencil, school } from 'ionicons/icons'
 import DashboardSchools from './views/DashboardSchools.vue'
 import EditSchools from './views/EditSchools.vue'
 import RegisterSchool from './views/RegisterSchool.vue'
 
 const routes = [
-  {
-    path: '/Schools/edit/:id',
-    name: 'EditSchools',
-    component: EditSchools,
-    moduleName: 'Schools',
-    moduleIcon: school,
-  },
   {
     path: '/Schools/list',
     name: 'InstitutionListInstitutions',
@@ -20,7 +13,21 @@ const routes = [
       moduleIcon: school,
       icon: school,
       name: 'Escolas',
-      order: 4,
+      order: 1,
+    },
+  },
+  {
+    path: '/Schools/edit/:id',
+    name: 'EditSchools',
+    component: EditSchools,
+    moduleName: 'Schools',
+    moduleIcon: school,
+    meta: {
+      moduleName: 'Schools',
+      moduleIcon: school,
+      icon: pencil,
+      name: 'Escolas EDICAO',
+      order: 1,
     },
   },
   {
@@ -30,7 +37,7 @@ const routes = [
     meta: {
       moduleName: 'Schools',
       moduleIcon: school,
-      icon: school,
+      icon: create,
       name: 'Cad Escola',
       order: 5,
     },
