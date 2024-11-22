30e9c174e49e544fa6265a2ceb2969cc5e455cb8
Jardel Filho <30330767+Jardeldev@users.noreply.github.com>
2024-11-22 13:51:41 -0300

    Revert "Design/login"

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import showToast from '@/utils/toast-alert'
-import { IonButton, IonIcon, IonInput, IonItem, IonList } from '@ionic/vue'
-import { eye, lockClosed, mail } from 'ionicons/icons'
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useUserStore } from '../store/user'
@@ -29,6 +27,8 @@ async function signIn() {
   })
   loading.value = false
 
+  router.push(`/`) // Enquanto o login não é feito, redireciona para a home para simular um login efetivo
+
   if (error) {
     showToast('Falha ao fazer login', 'top', 'warning')
     console.error('Erro ao fazer login:', error.message)
@@ -55,34 +55,31 @@ const navigation = {
     <ion-grid class="login-grid">
       <ion-row class="ion-justify-content-center ion-align-items-center full-height">
         <ion-col size="12" size-md="6" size-lg="4" class="login-form">
-          <ion-img src="/assets/EduPrimeChat.png" alt="Logo" class="login-logo" />
+          <ion-img src="/assets/logo.png" alt="Logo" class="login-logo" />
 
-          <IonItem>
-            <IonInput label-placement="stacked" label="Email" placeholder="seuemail@dominio.com">
-              <IonIcon slot="start" :icon="mail" aria-hidden="true" />
-            </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonInput type="password" label-placement="stacked" label="Senha" placeholder="Digite sua senha">
-              <IonIcon slot="start" :icon="lockClosed" aria-hidden="true" />
-              <ion-input-password-toggle slot="end" />
-            </IonInput>
-          </IonItem>
+          <ion-item>
+            <ion-input v-model="email" label-placement="stacked" label="Email" placeholder="seuemail@dominio.com" />
+          </ion-item>
+          <ion-item>
+            <ion-input v-model="password" label-placement="stacked" label="Senha" type="password" placeholder="Digite sua senha" />
+          </ion-item>
 
-          <ion-row class="ion-justify-content-start">
-            <ion-col size="auto">
-              <IonButton fill="clear" class="forgot-password-button">
+          <ion-button expand="block" @click="signIn">
+            <ion-spinner v-if="loading" slot="end" name="crescent" />
+            Entrar
+          </ion-button>
+
+          <ion-button expand="block" fill="outline" @click="navigation.goToSignUp">
+            Registrar
+          </ion-button>
+          <ion-row class="ion-justify-content-between">
+            <ion-col size="auto" />
+            <ion-col size="auto" class="ion-text-end">
+              <ion-button fill="outline" @click="navigation.resetPassword">
                 Esqueceu a senha?
-              </IonButton>
+              </ion-button>
             </ion-col>
           </ion-row>
-          <IonButton expand="block" class="login-button" @click="signIn">
-            Entrar
-          </IonButton>
-
-          <IonButton expand="block" class="register-button" color="tertiary" @click="navigation.goToSignUp">
-            Registrar
-          </IonButton>
         </ion-col>
       </ion-row>
     </ion-grid>
@@ -105,48 +102,17 @@ const navigation = {
   height: 100%;
 }
 
-.login-button {
-  margin-top: 20px;
-}
-
-.register-button {
-  margin-top: 10px;
-}
-
 .login-form {
   background-color: #fff;
   padding: 20px;
   border-radius: 10px;
-}
-
-.forgot-password-button {
-  text-transform: none;
-  box-shadow: none;
-  position: relative;
-  overflow: hidden;
-}
-
-.forgot-password-button::after {
-  content: '';
-  position: absolute;
-  bottom: 0; /* Posiciona a linha logo abaixo do texto */
-  left: 50%; /* Inicia a linha no centro horizontal do botão */
-  width: 0;
-  height: 2px; /* Espessura da linha */
-  background-color: #4f2974; /* Cor da linha */
-  transition: width 0.3s ease; /* Animação suave */
-  transform: translateX(-50%); /* Centraliza horizontalmente */
-  transform-origin: center; /* Faz a linha crescer para os dois lados */
-}
-
-.forgot-password-button:hover::after {
-  width: 100%; /* A linha expande para preencher a largura total */
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
 }
 
 .login-logo {
   display: block;
   margin: 0 auto 20px;
-  width: 100%;
+  width: 150px;
   height: auto;
 }
 </style>
