5b90bc45d9e56e87eed1c0277c9e3769ee847d56
Yohan Sobrinho <yohan.sobrinho@eduprime.chat>
2025-01-08 15:04:48 -0300

    feat: Update&refactor on Course module(Schema and .vue)

--- src/modules/school-management/services/CourseService.ts
+++ src/modules/school-management/services/CourseService.ts
@@ -1,10 +1,9 @@
+import type { Course } from '@prisma/client'
 import BaseService from '@/services/BaseService'
 
 const table = 'course' as const
 
-type CourseTable = typeof table
-
-export default class CourseService extends BaseService<CourseTable> {
+export default class CourseService extends BaseService<Course> {
   constructor() {
     super(table) // Passando o nome da tabela para a classe base
   }
